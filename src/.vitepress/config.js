91aa9d7a4144f6038a07861cc8a25a65c56d0e0c
Mengmiya1027 <16014480+mmy1027@user.noreply.gitee.com>
2026-07-27 22:03:06 +0800

    Initial commit

---
+++ src/.vitepress/config.js
@@ -0,0 +1,88 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "ZMdocs",
+  description: "万物生长",
+
+  themeConfig: {
+
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    editLink: {
+      pattern: 'https://github.com/Mengmiya1027/WGdocs-v2/edit/main/src/:path',
+      text: '在 GitHub 上编辑此页面'
+    },
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+
+    outline: {
+      label: '页面导航'
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium'
+      }
+    },
+
+    langMenuLabel: '多语言',
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '未找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            }
+          }
+        }
+      }
+    },
+    notFound: {
+      code: '404',
+      title: '页面未找到',
+      quote: '您访问的页面找不到啦~\n' +
+          '可以试试返回上个链接\n' +
+          '或者点击下面的按钮返回主页哦~',
+      linkLabel: '返回主页面',
+      linkText: '返回主页面'
+    }
+  }
+})
